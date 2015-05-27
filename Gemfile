3d6348dac30063dec4471efdb7e595f5e48ef4eb
Hon Lam <lamh85@gmail.com>
2015-05-27 13:37:18 -0700

    Fixed the 'create' methods in RSPEC

--- Gemfile
+++ Gemfile
@@ -54,9 +54,9 @@ group :development, :test do
 
   gem "letter_opener"
 
-  gem 'rspec-rails', '~> 3.0'
+  gem 'rspec-rails'
 
   # Documentation: http://www.rubydoc.info/gems/factory_girl/file/GETTING_STARTED.md
-  gem "factory_girl_rails", "~> 4.0"
+  gem "factory_girl_rails"
 end
 
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,21 +2,27 @@ require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
   let(:user) { create(:user) }
-  let(:post) { create(:post) }
-  let(:post_1 ) { create(:post, user: user)}
+  let(:post_1) { create(:post) }
+  let(:post_2) { create(:post, user: user)}
 
-  describe "#create: " do
-    context "user is signed in: " do
-      before { login(user) }
+  def login(user)
+    request.session[:user_id] = user.id
+  end
 
-      context "with valid parameters: " do
+  describe "#create" do
+    context "user is signed in" do
+
+      context "with valid parameters" do
 
         def valid_request
-          post :create, :post
+          post_1 # Calls the method let(:post_1)
+          user # Calls the method let(:user)
+          login(user)
         end
 
         it "creates a new post" do
           expect { valid_request }.to change {Post.count}.by(1)
+          puts "The first name of the post's user is... #{post_1.user.first_name}"
         end # it "creates a new post"
 
       end # context "with valid parameters"
